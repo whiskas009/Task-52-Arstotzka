ab872cdccf1d04841fdeb68354b2e96b9aaf3f72
Daniil Gorin <daniil009.gorin@yandex.ru>
2022-08-18 14:07:42 +0300

    Добавьте файлы проекта.

---
+++ Task 52 Arstotzka/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_52_Arstotzka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Prisoner> prisoners = new List<Prisoner>() {new Prisoner("Иван", "Коррпуция"),
+                                                             new Prisoner("Артём", "Вандализм"),
+                                                             new Prisoner("Денис", "Антиправительственное"),
+                                                             new Prisoner("Макс", "Шпионаж"),
+                                                             new Prisoner("Иван", "Антиправительственное"), };
+
+            Console.WriteLine("Старый список: ");
+
+            foreach (Prisoner prisoner in prisoners)
+            {
+                Console.WriteLine($"Имя: {prisoner.Name}. Преступление: {prisoner.Crime}");
+            }
+
+            var selectedPrisoners = from prisoner in prisoners
+                                    where prisoner.Crime != "Антиправительственное"
+                                    select prisoner;
+
+            prisoners = selectedPrisoners.ToList();
+
+            Console.WriteLine("Новый список: ");
+
+            foreach (Prisoner prisoner in prisoners)
+            {
+                Console.WriteLine($"Имя: {prisoner.Name}. Преступление: {prisoner.Crime}");
+            }
+        }
+    }
+
+    class Prisoner
+    {
+        public string Name { get; private set; }
+        public string Crime { get; private set; }
+
+        public Prisoner(string name, string crime)
+        {
+            Name = name;
+            Crime = crime;
+        }
+    }
+}
